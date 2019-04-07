8cbc62229f29e6bf11dcaca1fccde2bbadef0d23
Jesse Stratton <jgs.stratton@gmail.com>
2019-04-07 14:20:43 -0400

    React now setup for dev and production env

---
+++ client/index.js
@@ -0,0 +1,15 @@
+/**
+ * Use express to serve production build react files
+ */
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.REACT_APP_PORT || 3000;
+app.listen(PORT).on('error', console.log);
