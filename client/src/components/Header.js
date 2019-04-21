097a6c70c78f64641eb67f6e3e1727a9520266b1
Jesse Stratton <jgs.stratton@gmail.com>
2019-04-21 11:39:59 -0400

    Start building header

---
+++ client/src/components/Header.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavListLink = ({ to, children }) => (
+    <li className="nav-item">
+        <Link className="nav-link" to={to}>
+            {children}
+        </Link>
+    </li>
+);
+
+const NavDropLink = ({ to, children }) => (
+    <Link className="dropdown-item" to={to}>
+        {children}
+    </Link>
+);
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="bg-dark">
+                <div className="center-content">
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                        <div>
+                            <button
+                                className="navbar-toggler collapsed"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="##navbar"
+                            >
+                                <i className="fa fa-bars" />
+                            </button>
+                            <span className="navbar-brand">My checkbook</span>
+                        </div>
+                        <div className="text-right flex-nowrap d-flex d-md-none text-secondary">
+                            Summary -{' '}
+                            <span className="badge badge-primary text-nowrap">
+                                #moneyFormat(rc.user.getSummaryBalance())#
+                            </span>
+                        </div>
+                        <div className="navbar-collapse collapse" id="navbar">
+                            <ul className="navbar-nav mr-auto">
+                                <NavListLink to="#buildUrl('account.list')#">
+                                    Accounts
+                                </NavListLink>
+                                <NavListLink to="#buildUrl('transfer.new')#">
+                                    Transfer
+                                </NavListLink>
+                                <NavListLink to="#buildUrl('transactionSearch.search')#">
+                                    Search
+                                </NavListLink>
+                                <li className="nav-item dropdown">
+                                    <a
+                                        className="nav-link dropdown-toggle"
+                                        data-toggle="dropdown"
+                                    >
+                                        Other Features
+                                    </a>
+                                    <div className="dropdown-menu">
+                                        <NavDropLink to="#buildUrl('schedular.autoPaymentList')#">
+                                            Automatic Payments
+                                        </NavDropLink>
+                                        <NavDropLink to="#buildUrl('user.checkbookRounding')#">
+                                            Add Auto Rounding
+                                        </NavDropLink>
+                                        <NavDropLink to="#buildUrl('category.manageCategories')#">
+                                            Manage Categories
+                                        </NavDropLink>
+                                        <NavDropLink to="TRN_300.php">
+                                            Cost Breakdown
+                                        </NavDropLink>
+                                        {process.env.NODE_ENV ===
+                                            'development' && (
+                                            <NavDropLink to="#buildUrl('admin.devToggles')#">
+                                                Dev toggles
+                                            </NavDropLink>
+                                        )}
+                                    </div>
+                                </li>
+
+                                <NavListLink to="#buildUrl('auth.logout)#">
+                                    Logout
+                                </NavListLink>
+                            </ul>
+                        </div>
+                        <div className="text-right d-none d-md-inline text-secondary">
+                            Checkbook Summary -{' '}
+                            <span className="badge badge-primary">
+                                #moneyFormat(rc.user.getSummaryBalance())#
+                            </span>
+                        </div>
+                    </nav>
+
+                    <div className="title-bar">
+                        <hr />
+                        {/*
+                <cfif request.section eq "transaction" or (request.section eq 'transfer' and request.item eq 'edit')>
+                    #view('transaction/_header')#
+                </cfif>
+                */}
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
