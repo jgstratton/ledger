39f08d1a02f9faad09569e678ecf5c74da1c62be
Jesse Stratton <jgs.stratton@gmail.com>
2019-05-08 21:32:50 -0400

    Setup api proxy

--- client/index.js
+++ client/index.js
@@ -2,8 +2,13 @@
  * Use express to serve production build react files
  */
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const app = express();
 
+const PORT = process.env.REACT_PORT || 3000;
+const API_PORT = process.env.REACT_APP_API_URL || 3000;
+
+app.use(proxy('/api', { target: 'http://localhost:' + API_PORT }));
 app.use(express.static('build'));
 
 const path = require('path');
@@ -11,5 +16,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.REACT_PORT || 3000;
 app.listen(PORT).on('error', console.log);
