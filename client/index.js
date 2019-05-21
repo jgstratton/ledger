5b506c0fdb9a704c3409c7d23d15391d771dd7ef
Jesse Stratton <jgs.stratton@gmail.com>
2019-05-20 23:14:05 -0400

    Add https to express front end.

--- client/index.js
+++ client/index.js
@@ -4,15 +4,33 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 const app = express();
-
+const https = require('https');
+const fs = require('fs');
 const proxyUrl = 'http://lucee:8080/src/index.cfm';
+const sslEnabled = process.env.ENABLE_SSL || false;
+const port = process.env.REACT_PORT;
+const certFile = process.env.CERT_BASE_PATH + '/' + process.env.CERT_FILE;
+const certKey = process.env.CERT_BASE_PATH + '/' + process.env.CERT_KEY_FILE;
+const path = require('path');
 
 app.use(proxy('/api', { target: proxyUrl }));
 app.use(express.static('build'));
 
-const path = require('path');
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.REACT_PORT).on('error', console.log);
+if (sslEnabled) {
+    const options = {
+        key: fs.readFileSync(certKey),
+        cert: fs.readFileSync(certFile)
+    };
+    https
+        .createServer(options, app)
+        .listen(port)
+        .on('error', console.log);
+} else {
+    http.createServer(app)
+        .listen(port)
+        .on('error', console.log);
+}
