43c636a0af068df3c484f07e5967351280c7e6ff
Jesse Stratton <jgs.stratton@gmail.com>
2019-11-03 19:23:17 -0500

    Auto-format

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,8 +1,8 @@
-jConfirm = function(confirmMessage, fncOk, fncCancel){
+jConfirm = function(confirmMessage, fncOk, fncCancel) {
+    var fncCancel = fncCancel || function() {};
 
-    var fncCancel = fncCancel || function(){};
-
-    var modalString = `
+    var modalString =
+        `
         <div id="modalConfirmDiv" tabindex="-1" class="modal fade">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -13,7 +13,9 @@ jConfirm = function(confirmMessage, fncOk, fncCancel){
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>` + confirmMessage + `</p>
+                    <p>` +
+        confirmMessage +
+        `</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-success">Ok</button>
@@ -24,125 +26,148 @@ jConfirm = function(confirmMessage, fncOk, fncCancel){
         </div>`;
 
     $(modalString).appendTo('body');
-    
-    $("#modalConfirmDiv .btn-success").click(fncOk);
 
-    $("#modalConfirmDiv .btn-danger").click(function(){
+    $('#modalConfirmDiv .btn-success').click(fncOk);
+
+    $('#modalConfirmDiv .btn-danger').click(function() {
         fncCancel();
-        $("#modalConfirmDiv").modal('hide');
-    });
-    
-    $("#modalConfirmDiv").modal({
-        show:true
-    });
-    
-    $("#modalConfirmDiv").on('hidden.bs.modal', function () {
-        $(this).data('bs.modal', null).remove();
+        $('#modalConfirmDiv').modal('hide');
     });
 
-}
+    $('#modalConfirmDiv').modal({
+        show: true
+    });
+
+    $('#modalConfirmDiv').on('hidden.bs.modal', function() {
+        $(this)
+            .data('bs.modal', null)
+            .remove();
+    });
+};
 
 function getAlertIconHtml(alertType) {
     switch (alertType) {
-        case "danger":
-            return "fa fa-exclamation-triangle";
-        case "warning":
-            return "fa fa-exclamation-circle";
-        case "success":
-            return "fa fa-check";
+        case 'danger':
+            return 'fa fa-exclamation-triangle';
+        case 'warning':
+            return 'fa fa-exclamation-circle';
+        case 'success':
+            return 'fa fa-check';
         default:
-            return "";
+            return '';
     }
 }
 
 function getAlertHtml(alertType, alertTitle) {
-    return  `
+    return (
+        `
         <div>
-            <i class="` + getAlertIconHtml(alertType) + `"></i> 
-            ` + alertTitle + `
-        </div>`;
+            <i class="` +
+        getAlertIconHtml(alertType) +
+        `"></i> 
+            ` +
+        alertTitle +
+        `
+        </div>`
+    );
 }
 
 function getAlertErrorListHtml(alertErrors) {
     var errorListString = '<ul>';
-    for (var i=0; i < alertErrors.length; i++){
-        errorListString  = errorListString + '<li>' + alertErrors[i] + '</li>';
+    for (var i = 0; i < alertErrors.length; i++) {
+        errorListString = errorListString + '<li>' + alertErrors[i] + '</li>';
     }
     errorListString = errorListString + '</ul>';
     return errorListString;
 }
 
 function createAlert(alertType, alertTitle, alertErrors) {
-    return `
-        <div class="alert alert-` + alertType + `">
+    return (
+        `
+        <div class="alert alert-` +
+        alertType +
+        `">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-            ` + getAlertHtml(alertType,alertTitle) + getAlertErrorListHtml(alertErrors) + `
-        </div>`;
+            ` +
+        getAlertHtml(alertType, alertTitle) +
+        getAlertErrorListHtml(alertErrors) +
+        `
+        </div>`
+    );
 }
 
-function addSpinner($btn){
+function addSpinner($btn) {
     $btn.html($btn.html() + "<i class='fa fa-spinner fa-spin'></i>");
 }
 
-function removeSpinner($btn){
-    $btn.find(".fa-spinner").remove();
+function removeSpinner($btn) {
+    $btn.find('.fa-spinner').remove();
 }
 
-//format dollar amounts 
-function htmlFormatMoney(n) {
+//format dollar amounts
+function htmlFormatMoney(n, addSpan) {
     var c = 2,
-        dollarClass = n < 0 ? "dollar-negative" : "",
-        i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+        dollarClass = n < 0 ? 'dollar-negative' : '',
+        i = String(parseInt((n = Math.abs(Number(n) || 0).toFixed(c)))),
         j = (j = i.length) > 3 ? j % 3 : 0,
-        result = "$" + (j ? i.substr(0, j) +  "," : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "1" +  ",") + (c ? "." + Math.abs(n - i).toFixed(c).slice(2) : "");
+        result =
+            '$' +
+            (j ? i.substr(0, j) + ',' : '') +
+            i.substr(j).replace(/(\d{3})(?=\d)/g, '1' + ',') +
+            (c
+                ? '.' +
+                  Math.abs(n - i)
+                      .toFixed(c)
+                      .slice(2)
+                : '');
     return '<span class="' + dollarClass + '">' + result + '</span>';
-};
-  
+}
+
 //post form data using javascript
-post = function(path, formData){
+post = function(path, formData) {
+    var $form = $('<form />', { method: 'POST', action: path });
 
-    var $form = $("<form />", { method: 'POST', action: path });
-
-    for(var field in formData){
-        var $field = $("<input />", {type: "hidden", name: field, value: formData[field] });
+    for (var field in formData) {
+        var $field = $('<input />', { type: 'hidden', name: field, value: formData[field] });
         $form.append($field);
     }
 
-    $("body").append($form);
+    $('body').append($form);
     $form.submit();
-}
+};
 
-$("[data-datepicker]").datepicker().click(function(){
-    var $this = $(this);
-    if(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 768){
-        $this.prop('readonly',true);
-    } else {
-        $this.prop('readonly',false);
-    }
-    
-}).attr("autocomplete", "off");  
+$('[data-datepicker]')
+    .datepicker()
+    .click(function() {
+        var $this = $(this);
+        if (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 768) {
+            $this.prop('readonly', true);
+        } else {
+            $this.prop('readonly', false);
+        }
+    })
+    .attr('autocomplete', 'off');
 
 //create a jquery function similar to $.getJSON but have it use POST instead of GET
-postJSON = function(url, data, callback){
-   
+postJSON = function(url, data, callback) {
     $.ajax({
         url: url,
-        type: "POST",
-        dataType: "json",
-        data:data
+        type: 'POST',
+        dataType: 'json',
+        data: data
     })
-    .done(function(data){
-        callback(data);
-    })
-    .fail(function(a, b){
-        $("body").append(a.responseText);
-    });
-}
+        .done(function(data) {
+            callback(data);
+        })
+        .fail(function(a, b) {
+            $('body').append(a.responseText);
+        });
+};
 
 //Remove on-load highlights
-$(".temp-highlight-row.table-success").removeClass("table-success");
+$('.temp-highlight-row.table-success').removeClass('table-success');
 
 /* 
     Get elements by the data-js-hook attribute  (which can be a list of hooks)
@@ -150,18 +175,19 @@ $(".temp-highlight-row.table-success").removeClass("table-success");
      2. Only return results that match a list item
 */
 var jsHook = {
+    getElement: function(hookName) {
+        var $matchedElements = $('[data-js-hook*=' + hookName + ']');
 
-    getElement: function(hookName){
-        var $matchedElements = $("[data-js-hook*=" + hookName + "]");
-        
-        return $matchedElements.filter(function(){
-            var hooks = $(this).data('jsHook').split(",");
-            for( var i = 0; i < hooks.length; i++ ){
-                if(hooks[i] == hookName){
+        return $matchedElements.filter(function() {
+            var hooks = $(this)
+                .data('jsHook')
+                .split(',');
+            for (var i = 0; i < hooks.length; i++) {
+                if (hooks[i] == hookName) {
                     return true;
                 }
             }
             return false;
         });
     }
-}
+};
